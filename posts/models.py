c904c4a0ef7fcb1bbd26da4944c6fab68a7a3cc7
EdwardShanahan07 <>
2023-10-17 17:33:31 +0000

    Create, Delete and, Put posts
    - Created PostList and PostDetail views
    - Created PostSerializer
    - Add posts to drf_api urls

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Post(models.Model):
+    """ Post Model """
+    
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    content = models.TextField(max_length=255)
+    image = models.ImageField(
+        upload_to='images/', blank=True
+    )
+    tag = models.CharField(max_length=25, blank=True)
+
+    
+    class Meta:
+        ordering = ['-created_at']
+        
+    def __str__(self):
+        return f"{self.id} {self.owner}"
+
