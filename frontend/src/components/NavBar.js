52758066f5809dea69f46fc19387836bf8f5d63f
EdwardShanahan07 <>
2023-10-18 17:43:21 +0000

    Add NavBar component

---
+++ frontend/src/components/NavBar.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
+const NavBar = () => {
+    return (
+       <Navbar expand="lg" className="bg-body-tertiary">
+        <Container>
+            <Navbar.Brand className="fw-bold" href="#home">
+                GGN
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto">
+                <Nav.Link href="#home">Discover</Nav.Link>
+                <Nav.Link href="#link">Sign In</Nav.Link>
+                <Nav.Link href="#link">Sign Up</Nav.Link>
+            </Nav>
+            </Navbar.Collapse>
+        </Container>
+    </Navbar>
+    )
+}
+
+export default NavBar;
