4416da9b72e6adef9fe6e017cce29ffd45f789a1
EdwardShanahan07 <>
2023-10-24 19:55:17 +0000

    Add PostEditForm component
    - PostEditForm is only accessible  to user who own the post
    - The user can change the content of the post

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,6 +9,7 @@ import SignIn from './pages/auth/SignIn';
 import PostCreateForm from './pages/posts/PostCreateForm';
 import PostPage from "./pages/posts/PostPage";
 import PostsPage from "./pages/posts/PostsPage";
+import PostEditForm from "./pages/posts/PostEditForm";
 
 import { useCurrentUser } from "./contexts/CurrentUserContext";
 
@@ -56,6 +57,7 @@ function App() {
                     />
                     <Route exact path="/posts/create" render={() => <PostCreateForm />} />
                     <Route exact path="/posts/:id" render={() => <PostPage />} />
+                    <Route exact path="/posts/:id/edit" render={() => <PostEditForm />} />
                     <Route render={() => <p>Page not found!</p>} />
                 </Switch>
             </Container>
