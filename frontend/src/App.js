6ad4c3091c04624c47012a278bbea51675ec34e7
EdwardShanahan07 <>
2023-10-26 13:10:21 +0000

    Add profile edits component
    - Add ProfileEditForm to edit profile details
    - Add UsernameForm to change username
    - Add UserPasswordForm to change password

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,10 +11,11 @@ import PostPage from "./pages/posts/PostPage";
 import PostsPage from "./pages/posts/PostsPage";
 import PostEditForm from "./pages/posts/PostEditForm";
 import ProfilePage from './pages/profiles/ProfilePage';
+import UsernameForm from "./pages/profiles/UsernameForm";
+import UserPasswordForm from "./pages/profiles/UserPasswordForm";
+import ProfileEditForm from "./pages/profiles/ProfileEditForm";
 
 import { useCurrentUser } from "./contexts/CurrentUserContext";
-
-
 import Container from "react-bootstrap/Container";
 
 
@@ -60,6 +61,21 @@ function App() {
                     <Route exact path="/posts/:id" render={() => <PostPage />} />
                     <Route exact path="/posts/:id/edit" render={() => <PostEditForm />} />
                     <Route exact path="/profiles/:id" render={() => <ProfilePage />} />
+                    <Route
+                        exact
+                        path="/profiles/:id/edit/username"
+                        render={() => <UsernameForm />}
+                    />
+                    <Route
+                        exact
+                        path="/profiles/:id/edit/password"
+                        render={() => <UserPasswordForm />}
+                    />
+                    <Route
+                        exact
+                        path="/profiles/:id/edit"
+                        render={() => <ProfileEditForm />}
+                    />
                     <Route render={() => <p>Page not found!</p>} />
                 </Switch>
             </Container>
