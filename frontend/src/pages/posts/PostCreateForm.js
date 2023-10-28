0da65af1758e03182f3060ee9d93723c7d856bb9
EdwardShanahan07 <>
2023-10-28 14:55:25 +0000

    Add styles to PostCreate component

--- frontend/src/pages/posts/PostCreateForm.js
+++ frontend/src/pages/posts/PostCreateForm.js
@@ -2,14 +2,18 @@ import React, { useRef, useState } from "react";
 
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
-// import Row from "react-bootstrap/Row";
-// import Col from "react-bootstrap/Col";
-// import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Container from "react-bootstrap/Container";
 import Alert from "react-bootstrap/Alert";
 import Image from "react-bootstrap/Image";
 
 import Upload from "../../assets/upload.png";
 import Asset from "../../components/Asset";
+
+import styles from "../../styles/PostCreateForm.module.css";
+import appStyles from "../../App.module.css";
+import btnStyles from "../../styles/Button.module.css";
 
 import { useHistory } from "react-router";
 import { axiosReq } from "../../api/axiosDefaults";
@@ -68,9 +72,9 @@ const PostCreateForm = () => {
         }
     };
 
-    return (
-        <Form onSubmit={handleSubmit}>
-            <Form.Group>
+    const textFields = (
+        <div className="text-center">
+            <Form.Group className="mb-3">
                 <Form.Label>Post</Form.Label>
                 <Form.Control
                     as="textarea"
@@ -87,7 +91,7 @@ const PostCreateForm = () => {
                     {message}
                 </Alert>
             ))}
-            <Form.Group>
+            <Form.Group className="mb-3">
                 <Form.Label>Tag</Form.Label>
                 <Form.Control
                     type="text"
@@ -103,47 +107,71 @@ const PostCreateForm = () => {
                 </Alert>
             ))}
 
-            <Form.Group className="text-center">
-                {image ? (
-                    <>
-                        <figure>
-                            <Image src={image} rounded />
-                        </figure>
-                        <div>
-                            <Form.Label
-                                className={` btn`}
-                                htmlFor="image-upload"
-                            >
-                                Change the image
-                            </Form.Label>
-                        </div>
-                    </>
-                ) : (
-                    <Form.Label
-                        className="d-flex justify-content-center"
-                        htmlFor="image-upload"
-                    >
-                        <Asset
-                            src={Upload}
-                            message="Click or tap to upload an image"
-                        />
-                    </Form.Label>
-                )}
+            <Button
+                className={`btn-secondary`}
+                onClick={() => history.goBack()}
+            >
+                cancel
+            </Button>
+            <Button className={`btn-primary`} type="submit">
+                create
+            </Button>
+        </div>
+    );
 
-                <Form.File
-                    id="image-upload"
-                    accept="image/*"
-                    onChange={handleChangeImage}
-                    ref={imageInput}
-                />
-            </Form.Group>
-            {errors?.image?.map((message, idx) => (
-                <Alert variant="warning" key={idx}>
-                    {message}
-                </Alert>
-            ))}
+    return (
+        <Form onSubmit={handleSubmit}>
+            <Row>
+                <Col className="py-2 p-0 p-md-2" md={7} lg={8}>
+                    <Container className={`${appStyles.Content} ${styles.Container} d-flex flex-column justify-content-center`}>
+                        <Form.Group className="text-center">
+                            {image ? (
+                                <>
+                                    <figure>
+                                        <Image className={appStyles.Image} src={image} rounded />
+                                    </figure>
+                                    <div>
+                                        <Form.Label
+                                            className={`${btnStyles.Button} ${btnStyles.Blue} btn`}
+                                            htmlFor="image-upload"
+                                        >
+                                            Change the image
+                                        </Form.Label>
+                                    </div>
+                                </>
+                            ) : (
+                                <Form.Label
+                                    className="d-flex justify-content-center"
+                                    htmlFor="image-upload"
+                                >
+                                    <Asset
+                                        src={Upload}
+                                        message="Click or tap to upload an image"
+                                    />
+                                </Form.Label>
+                            )}
 
-            <Button type="submit">Share</Button>
+                            <Form.File
+                                id="image-upload"
+                                accept="image/*"
+                                onChange={handleChangeImage}
+                                ref={imageInput}
+                            />
+                        </Form.Group>
+                        {errors?.image?.map((message, idx) => (
+                            <Alert variant="warning" key={idx}>
+                                {message}
+                            </Alert>
+                        ))}
+
+                        <div className="d-md-none">{textFields}</div>
+                    </Container>
+                </Col>
+
+                <Col md={5} lg={4} className="d-none d-md-block p-0 p-md-2">
+                    <Container>{textFields}</Container>
+                </Col>
+            </Row>
         </Form>
     );
 };
