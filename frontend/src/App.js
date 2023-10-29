aabcca102ddb813ece2c14f03d9163ca1608a195
EdwardShanahan07 <>
2023-10-29 16:18:30 +0000

    Add NotFound component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,6 +14,7 @@ import ProfilePage from './pages/profiles/ProfilePage';
 import UsernameForm from "./pages/profiles/UsernameForm";
 import UserPasswordForm from "./pages/profiles/UserPasswordForm";
 import ProfileEditForm from "./pages/profiles/ProfileEditForm";
+import NotFound from "./components/NotFound";
 
 import { useCurrentUser } from "./contexts/CurrentUserContext";
 import Container from "react-bootstrap/Container";
@@ -76,7 +77,7 @@ function App() {
                         path="/profiles/:id/edit"
                         render={() => <ProfileEditForm />}
                     />
-                    <Route render={() => <p>Page not found!</p>} />
+                    <Route render={() => <NotFound />} />
                 </Switch>
             </Container>
         </div >
---
+++ frontend/src/components/NotFound.js
@@ -0,0 +1,23 @@
+import react from "react";
+import NotFoundImage from "../assets/not-found.svg";
+import { Image, Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import styles from "../styles/NotFound.module.css";
+
+const NotFound = () => {
+    return (
+        <div className='text-center mt-5'>
+            <Image className={`mb-3 ${styles.Image}`} src={NotFoundImage} />
+
+            <p className='mb-3'>Sorry we could'nt find the page you are looking for!</p>
+
+            <Link to="/discover">
+                <Button className="btn-primary">
+                    Back To Home
+                </Button>
+            </Link>
+        </div>
+    );
+};
+
+export default NotFound;
